576eccd06d5a1f7b8d05d4efe4e559c5e98bc46d
Hugo Iteprat <hugoiteprat@gmail.com>
2019-10-25 16:37:24 +0200

    Ajout classe vide d'article

--- WEB/mvc/Model/Sortie.class.php
+++ WEB/mvc/Model/Sortie.class.php
@@ -1,4 +1,6 @@
 <?php
+
+require_once("../Model/Utilisateur.class.php");
   class Sortie{
     private $nom;
     private $date;
@@ -12,10 +14,47 @@
       $this->nom = $nom;
       $this->date = $date;
       $this->utilisateur = $utilisateur;
+      $this->adresse = $adresse;
+      $this->image = $image;
+      $this->pratique = $pratique;
+      $this->valide = $valide;
     }
   }
 
+  function getNom(){
+    return $this->nom;
+  }
 
+  function getDate(){
+    return $this->date;
+  }
 
+  function getUtilisateur(){
+    return $this->utilisateur;
+  }
+
+  function getAdresse(){
+    return $this->adresse;
+  }
+
+  function getImage(){
+    return $this->image;
+  }
+
+  function getPratique(){
+    return $this->pratique;
+  }
+
+  function getValide(){
+    return $this->valide;
+  }
+
+  function setPratique($pratique){
+    $this->pratique = $pratique;
+  }
+
+  function setValide($valide){
+    $this->valide = $valide;
+  }
 
  ?>
