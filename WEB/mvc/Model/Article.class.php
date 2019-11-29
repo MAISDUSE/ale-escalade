d55250cdd1373acd4221a8cf624323e10dd85a5e
ITEPRAT Hugo <iteprath@hostname.iut2.upmf-grenoble.fr>
2019-11-29 16:27:51 +0100

    Article

--- WEB/mvc/Model/Article.class.php
+++ WEB/mvc/Model/Article.class.php
@@ -8,11 +8,34 @@ class Article
   private $image;
   private $nbPieceJointe;
 
-  function __construct($titre, $contenu)
+  function __construct($titre, $contenu, $image, $nbPieceJointe)
   {
     $this->titre = $titre;
     $this->contenu = $contenu;
+    $this->image = $image;
+    $this->nbPieceJointe = $nbPieceJointe;
   }
+
+  function getTitre(){
+    return $this->titre;
+  }
+
+  function getContenu(){
+    return $this->contenu;
+  }
+
+  function getImage(){
+    return $this->image;
+  }
+
+  function getNbPiecejointe(){
+    return $this->nbPieceJointe;
+  }
+
+  function setNbPieceJointe($nb){
+    $this->nbPieceJointe = $nb;
+  }
+
 }
 
 
