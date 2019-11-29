ea01bebcf6bbae18e336a1c8526df1c776fc7e6a
xiensy <richomme.lucas@gmail.com>
2019-11-29 15:09:26 +0100

    User class

--- WEB/mvc/Model/Enum/TypeLicence.enum.php
+++ WEB/mvc/Model/Enum/TypeLicence.enum.php
@@ -1,8 +1,8 @@
 <?php
 class TypeLicence extends SplEnum{
-  const Jeune = 1;
-  const Adulte = 2;
-  const Famille = 3;
+  const J = 1;
+  const A = 2;
+  const F = 3;
 }
 
  ?>
--- WEB/mvc/Model/Utilisateur.class.php
+++ WEB/mvc/Model/Utilisateur.class.php
@@ -20,7 +20,38 @@ class Utilisateur{
 
 function __construct(int $numero, string $licence, TypeLicence $typeLicence, string $nom, string $prenom, Genre $genre, string $dateNaissance,
   string $adresse, string $numTel, string $numFix, string $adresseMail, Role $role, string $codeUtilisateur, Passeport $passeport){
-    
+
+    if(isset($numero)){
+      $this->numero=$numero;
+    }
+
+    $this->licence=$licence;
+    $this->typeLicence=$typeLicence;
+    $this->nom=$nom;
+    $this->prenom=$prenom;
+    $this->genre=$genre;
+    $this->dateNaissance=$dateNaissance;
+    $this->adresse=$adresse;
+    $this->numTel=$numTel;
+    $this->numFix=$numFix;
+    $this->adresseMail=$adresseMail;
+    $this->role=$role;
+    $this->codeUtilisateur=$codeutilisateur;
+    $this->passeport=$passeport;
+
+    assert(isset($this->numero));
+    assert(isset($this->licence));
+    assert(isset($this->typeLicence));
+    assert(isset($this->nom));
+    assert(isset($this->prenom));
+    assert(isset($this->genre));
+    assert(isset($this->dateNaissance));
+    assert(isset($this->adresse));
+    assert(isset($this->numTel)||isset($this->adresseMail));
+    assert(isset($this->role));
+    assert(isset($this->codeUtilisateur));
+    assert(isset($this->passeport));
+
 }
 
  ?>
