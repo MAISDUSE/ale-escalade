c78ad5fb66b76ba01f47e539dcbffac128fcecf7
ITEPRAT Hugo <iteprath@hostname.iut2.upmf-grenoble.fr>
2019-11-29 13:37:45 +0100

    Début forum sujet fait !

---
+++ WEB/mvc/Model/Sujet.class.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ *
+ */
+class Sujet{
+
+  private $titre;
+  private $description;
+  private $nomUtilisateur;
+  private $date;
+  private $evenement;
+
+  function __construct($titre, $description, $nomUtilisateur, $date, $evenement=NULL){
+    $this->$titre = $titre;
+    $this->description = $description;
+    $this->nomUtilisateur = $nomUtilisateur;
+    $this->date = $date;
+  }
+
+  function getTitre(){
+    return $this->titre;
+  }
+
+  function getDescription(){
+    return $this->description;
+  }
+
+  function getNomUtilisateur(){
+    return $this->nomUtilisateur;
+  }
+
+  function getDate(){
+    return $this->date;
+  }
+
+  function getEvenement(){
+    return $this->evenement;
+  }
+
+  function setEvenement($evenement){
+    $this->evenement = $evenement;
+  }
+}
+
+
+
+
+ ?>
---
+++ WEB/mvc/View/Forum.view.php
@@ -0,0 +1,27 @@
+<?php
+  
+
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Forum ALE Escalade</title>
+  </head>
+  <body>
+    <header>
+      <h1>Bienvenue sur le forum</h1>
+    </header>
+
+    <div class="">
+      <h2>Liste des sujets de discussions</h2>
+
+      <article class="">
+
+      </article>
+
+    </div>
+  </body>
+</html>
