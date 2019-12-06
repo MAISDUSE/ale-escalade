0530e95eb7c0c764e1906338f0f19b139d570339
dusema <dusema@hostname.iut2.upmf-grenoble.fr>
2019-12-06 15:21:49 +0100

    dao

--- WEB/mvc/Model/DAO.class.php
+++ WEB/mvc/Model/DAO.class.php
@@ -1,16 +1,16 @@
 <?php
-<<<<<<< HEAD
+
   require_once("../Model/Article.class.php");
   require_once("../Model/Certificat.class.php");
   require_once("../Model/Contact.class.php");
   require_once("../Model/Sortie.class.php");
   require_once("../Model/Sujet.class.php");
   require_once("../Model/Utilisateur.class.php");
-=======
+
 
 
 class ClassName{
-  private $path; 
+  private $path;
 
   function __construct(){
 
@@ -23,7 +23,5 @@ class ClassName{
 
 
 
-
->>>>>>> 2370478ee4fb868f69e1828b709813dbeac6a793
 
  ?>
