7550a3df5eda1c46036395a05ed2580dc2e4a0c4
DOMINGO Yoann <domingoy@hostname.iut2.upmf-grenoble.fr>
2019-12-06 16:42:23 +0100

    test

---
+++ WEB/mvc/test/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Site web de l'Ale-Escalade</title>
+    <script>
+    /*var looper;
+    var degrees = 0;
+    var pos = 0;
+    function rotateAnimation(el,speed){
+    	var elem = document.getElementById(el);
+    	elem.style.MozTransform = "rotate("+degrees+"deg)";
+    	looper = setTimeout('rotateAnimation(\''+el+'\','+speed+')',speed);
+    	degrees++;
+    	if(degrees >= 180){
+    		degrees = 180;
+          if (pos == 300) {
+            elem.style.top = pos + 2*window.scrollY + 'px';
+          } else {
+            pos++;
+            elem.style.top = pos + 'px';
+          }
+    }
+  }
+
+    	//document.getElementById("status").innerHTML = "rotate("+degrees+"deg)";
+      */
+    </script>
+    <link rel="stylesheet" href="header.css">
+  </head>
+  <body>
+    <header>
+      <nav>
+          <li><img src="../Ressources/Ale_black.png" alt="TEST" id="1"></li>
+          <li><h1>ALE-ESCALADE</h1></li>
+
+          <li class="push"><a href="../View/Accueil.view.php">Accueil</a></li>
+          <li><a href="#">Planning</a></li>
+          <li><a href="../View/Inscription.view.php">Inscription</a></li>
+          <li><a href="#">Connexion</a></li>
+          <li><a href="#">Forum</a></li>
+          <li><a href="../View/Contact.view.php">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+
+  </body>
+</html>
