f5e7aa7839f4a44a0d7ec4318132cf02c476261a
dusema <dusema@hostname.iut2.upmf-grenoble.fr>
2019-12-06 15:02:32 +0100

    t

--- WEB/mvc/Controler/SujetForum.ctrl.php
+++ WEB/mvc/Controler/SujetForum.ctrl.php
@@ -13,22 +13,30 @@
 
 
 <?php
-//methode pour recup le Sujet a partir de son titre
+//methode pour recup le Sujet a partir de son id
 require_once("../Model/Sujet.class.php");
 $sujet1 = new Sujet(1,"Test","Un petit s'est fait gommer il y aurait pas de message bande de fils de pute ça va bande","Darkos", date("d/m/Y"));
  ?>
-
-
-
-    <aside>
-      <h4><?=$sujet1->getTitre()?></h4>
-    </aside>
-
+    <h4><?=$sujet1->getTitre()?></h4>
     <p class="date">Posté le : <?=$sujet1->getDate()?></p>
     <p class="description">Description : <?=$sujet1->getDescription()?></p>
+    <p class="description">Créateur : <?=$sujet1->getNomUtilisateur()?></p>
 
 
+ <?php //zone des commentaire
+//recup tout les commentairers depuis id trié par date, plus recent d'abord
+// $liste = getCommentaires($Sujet1->getId());
+
+//affiche
+?>
+<!-- zone poste un commentaire !-->
+<br>
+<form class="" action="SujetForum.ctrl.php" method="post">
 
 
+<label for="Comment">Veuillez Commenter</label>
+<input type="textfield" name="Comment" placeholder="Commenter">
+<input type="submit" name="Envoyer" value="Envoyer">
+</form>
 </body>
 </html>
