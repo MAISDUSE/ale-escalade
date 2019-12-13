8e9cd04af517aacc401e2f0e58d84019295bcfa8
dusema <dusema@hostname.iut2.upmf-grenoble.fr>
2019-12-13 15:54:46 +0100

    sujet forum

--- WEB/mvc/Controler/SujetForum.ctrl.php
+++ WEB/mvc/Controler/SujetForum.ctrl.php
@@ -11,32 +11,64 @@
     <h1><?=$titre?></h1>
   </header>
 
+  <?php
+  require_once("../Model/Sujet.class.php");
+  require_once("../Model/Commentaire.class.php");
 
-<?php
-//methode pour recup le Sujet a partir de son id
-require_once("../Model/Sujet.class.php");
-$sujet1 = new Sujet(1,"Test","Un petit s'est fait gommer il y aurait pas de message bande de fils de pute ça va bande","Darkos", date("d/m/Y"));
- ?>
+
+  //methode pour recup le Sujet a partir de son id passé en get
+  $sujet1 = new Sujet(1,"Test","Un petit s'est fait gommer il y aurait pas de message bande de fils ","Darkos", date("d/m/Y"));
+///////////////////////////////////////////////////////
+
+  //recup tout les commentairers depuis id trié par date, plus recent d'abord
+  // $liste = getCommentaires($Sujet1->getId());
+
+  //test 1 comentaire random
+  $comm= new Commentaire(666,1,date(DATE_RSS),"Ah mais ok dac");
+  $liste = array($comm,$comm );
+
+///////////////////////////////////////////////////////////////
+  //recup le commentaire du form
+  if (isset($_POST['Comment'])){
+    //AJOUT DANS BDD
+    //EX
+  $comm2 = new commentaire(777,1,date(DATE_RSS),$_POST['Comment']);
+  array_push($liste,$comm2);
+  }
+
+  ?>
+  <div class="sujet">
     <h4><?=$sujet1->getTitre()?></h4>
     <p class="date">Posté le : <?=$sujet1->getDate()?></p>
-    <p class="description">Description : <?=$sujet1->getDescription()?></p>
     <p class="description">Créateur : <?=$sujet1->getNomUtilisateur()?></p>
+    <p class="description">Description : <?=$sujet1->getDescription()?></p>
 
-
- <?php //zone des commentaire
-//recup tout les commentairers depuis id trié par date, plus recent d'abord
-// $liste = getCommentaires($Sujet1->getId());
-
-//affiche
-?>
-<!-- zone poste un commentaire !-->
+  </div>
 <br>
-<form class="" action="SujetForum.ctrl.php" method="post">
 
 
-<label for="Comment">Veuillez Commenter</label>
-<input type="textfield" name="Comment" placeholder="Commenter">
-<input type="submit" name="Envoyer" value="Envoyer">
-</form>
-</body>
-</html>
+  <?php //zone des commentaire
+
+  //affiche les commentairers
+  ?>
+  <div class="commentaires">
+    <?php foreach ($liste as $value) {
+      ?>
+      <p><?=$value->getDate()?></p>
+      <p><?=$value->getNumAuteur()?></p>
+      <p><?=$value->getContenu()?></p>
+      <br>
+
+      <?php  } ?>
+
+    </div>
+
+    <!-- zone poste un commentaire !-->
+    <br>
+    <form class="" action="SujetForum.ctrl.php" method="post">
+      <label for="Comment">Veuillez Commenter</label>
+      <input type="textfield" name="Comment" placeholder="Commenter">
+      <input type="submit" name="Envoyer" value="Envoyer">
+    </form>
+  </body>
+  </html>
