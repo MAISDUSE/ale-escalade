fea04bc78d2ceae046246f1ec7a3ee82730f9501
HugoDeku <hucureuil@gmail.com>
2019-12-13 15:24:55 +0100

    Header mis en place

--- WEB/mvc/View/Accueil.view.php
+++ WEB/mvc/View/Accueil.view.php
@@ -7,35 +7,7 @@
     <link rel="stylesheet" href="../View/Design/Accueil.css">
   </head>
   <body>
-    <header>
-      <div class="menu-toggle" id="hamburger">
-          <i class="fas fa-bars"></i>
-      </div>
-      <div class="overlay"></div>
-      <div class="container">
-          <nav>
-              <h1 class="brand"><a href="../Controler/Accueil.ctrl.php"><img src="../Ressources/Ale_black.png" alt="Logo Ale"></h1>
-              <ul>
-                  <li><a href="../Controler/Accueil.ctrl.php">Accueil</a></li>
-                  <li><a href="../Controler/Accueil.ctrl.php">Planning</a></li>
-                  <li><a href="../Controler/Inscription.ctrl.php">Inscription</a></li>
-                  <li><a href="../Controler/Connexion.ctrl.php">Connexion</a></li>
-                  <li><a href="../Controler/Forum.ctrl.php">Forum</a></li>
-                  <li><a href="../Controler/Contact.ctrl.php">Contact</a></li>
-              </ul>
-          </nav>
-      </div>
-      <!-- Bouton de scrolling animé via CSS (NE PAS TOUCHER) -->
-      <div class="box">
-        <a href="#SectionArticle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </a>
-      </div>
-      <!--FIN DE LA SECTION DU BOUTON DE SCROLLING-->
-
-    </header>
+    <?php include "Design/Header.view.php"; ?>
     <section id="SectionArticle">
       <div class="Article">
         <div class="Titre">
--- WEB/mvc/View/OutilAdministratif.view.php
+++ WEB/mvc/View/OutilAdministratif.view.php
@@ -7,41 +7,9 @@
     <link rel="stylesheet" href="../View/Design/Inscription.css">
   </head>
   <body>
-    <header>
-      <div class="menu-toggle" id="hamburger">
-          <i class="fas fa-bars"></i>
-      </div>
-      <div class="overlay"></div>
-      <div class="container">
-          <nav>
-              <h1 class="brand"><a href="../Controler/Accueil.ctrl.php"><img src="../Ressources/Ale_black.png" alt="Logo Ale"></h1>
-              <ul>
-                  <li><a href="../Controler/Accueil.ctrl.php">Accueil</a></li>
-                  <li><a href="../Controler/Accueil.ctrl.php">Planning</a></li>
-                  <li><a href="../Controler/Inscription.ctrl.php">Inscription</a></li>
-                  <li><a href="../Controler/Connexion.ctrl.php">Connexion</a></li>
-                  <li><a href="../Controler/Forum.ctrl.php">Forum</a></li>
-                  <li><a href="../Controler/Contact.ctrl.php">Contact</a></li>
-              </ul>
-          </nav>
-      </div>
-    <script>
-    function myMove() {
-      var elem = document.getElementById("myAnimation");
-      var pos = 0;
-      var id = setInterval(frame, 10);
-      function frame() {
-        if (pos == 180) {
-          clearInterval(id);
-        } else {
-          pos++;
-          elem.style.top = pos + 'px';
-
-        }
-      }
-    }
-    </script>
+    <?php include "Design/Header.view.php"; ?>
     <div class="elements">
+
       <article class="">
         <h2>Inscriptions en attente</h2>
 
@@ -63,7 +31,5 @@
       </article>
 
     </div>
-  </header>
-    <br><br><br><br><br>
   </body>
 </html>
