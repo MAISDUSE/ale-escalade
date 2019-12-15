15f7d2821444b8555aa2a8b68a9c5c569640f1c0
gherrazs <salah.gherraz@iut2.univ-grenoble-alpes.fr>
2019-12-15 01:11:32 +0100

    Responsivité début

--- WEB/mvc/View/Design/Header.view.php
+++ WEB/mvc/View/Design/Header.view.php
@@ -1,22 +1,31 @@
+
+
+
+
 <header>
   <div class="menu-toggle" id="hamburger">
-      <i class="fas fa-bars"></i>
+    <label for="toggle">&#9766;</label>
   </div>
   <div class="overlay"></div>
-  <div id="menu" class="container">
-      <nav>
-          <h1 class="brand"><a href="../Controler/Accueil.ctrl.php"><img src="../Ressources/Ale_black.png" alt="Logo Ale"></h1>
-          <ul>
-              <li><a href="../Controler/Accueil.ctrl.php">Accueil</a></li>
-              <li><a href="../Controler/Planning.ctrl.php">Planning</a></li>
-              <li><a href="../Controler/Inscription.ctrl.php">Inscription</a></li>
-              <li><a href="../Controler/Connexion.ctrl.php">Connexion</a></li>
-              <li><a href="../Controler/Forum.ctrl.php">Forum</a></li>
-              <li><a href="../Controler/Contact.ctrl.php">Contact</a></li>
+  <div class="nav">
+    <label for="toggle">&#9776;</label>
+    <input type="checkbox" id="toggle">
+    <div id="menu" class="container">
+        <nav>
+            <h1 class="brand"><a href="../Controler/Accueil.ctrl.php"><img src="../Ressources/Ale_black.png" alt="Logo Ale"></h1>
+            <ul>
+                <li><a href="../Controler/Accueil.ctrl.php">Accueil</a></li>
+                <li><a href="../Controler/Planning.ctrl.php">Planning</a></li>
+                <li><a href="../Controler/Inscription.ctrl.php">Inscription</a></li>
+                <li><a href="../Controler/Connexion.ctrl.php">Connexion</a></li>
+                <li><a href="../Controler/Forum.ctrl.php">Forum</a></li>
+                <li><a href="../Controler/Contact.ctrl.php">Contact</a></li>
 
-          </ul>
-      </nav>
+            </ul>
+        </nav>
+    </div>
   </div>
+
 
 
 </header>
