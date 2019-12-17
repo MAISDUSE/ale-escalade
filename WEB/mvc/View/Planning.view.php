318e07330d775b604ab4ccfce011a78fb56d5042
Alexandre71260 <alexandredavid.71260@gmail.com>
2019-12-17 14:05:13 +0100

    .

--- WEB/mvc/View/Planning.view.php
+++ WEB/mvc/View/Planning.view.php
@@ -13,13 +13,9 @@
       <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23EF6C00&amp;ctz=Europe%2FParis&amp;src=MmpzMzhoMWFiamNnYTQxYjlvOG1zb2czaHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%238A2D38&amp;showTz=0&amp;showCalendars=0&amp;showTabs=1&amp;showNav=1&amp;showDate=1&amp;mode=WEEK" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </div>
 
-<<<<<<< HEAD
     <div class="agenda">
       //mettre un agenda PUBLIC afin de permettre la mise à jour par les utilisateurs
     </div>
-=======
-
->>>>>>> 95f5e1ef30d1294d6a40c9dc85bacde5f2c498a2
 
     <?php include "Design/Footer.view.php"; ?>
   </body>
