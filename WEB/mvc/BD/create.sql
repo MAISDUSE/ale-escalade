46cbf8792f2c619f1ed5905efa125c1d97e2eeaa
xiensy <richomme.lucas@gmail.com>
2019-12-20 13:38:12 +0100

    maj create (aled)

--- WEB/mvc/BD/create.sql
+++ WEB/mvc/BD/create.sql
@@ -23,8 +23,7 @@ CREATE TABLE IF NOT EXISTS Utilisateur(
   CodeUtilisateur TEXT,
   Passeport ENUM('Blanc', 'Jaune', 'Orange', 'Vert', 'Bleu', 'RougePerf', 'RougeExt','Noir'),
   Contact INT,
-  CONSTRAINT `fk_contact_contactid`
-    FOREIGN KEY (Contact) REFERENCES Contacts (ID)
+  CONSTRAINT FOREIGN KEY (Contact) REFERENCES Contacts(ID)
 );
 
 CREATE TABLE IF NOT EXISTS CompteRendu(
