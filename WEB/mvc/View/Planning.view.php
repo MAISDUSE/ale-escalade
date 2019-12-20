eaf1511c461b3112500989e31ad3a70ba17db276
Alexandre71260 <alexandredavid.71260@gmail.com>
2019-12-20 13:44:55 +0100

    Redefinition de la page Planning.view.php avec les agendas des cours ET des événements

--- WEB/mvc/View/Planning.view.php
+++ WEB/mvc/View/Planning.view.php
@@ -10,11 +10,11 @@
     <?php include "Design/Header.view.php"; ?>
 
     <div class="agenda"> <!-- Agenda des cours -->
-      <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23EF6C00&amp;ctz=Europe%2FParis&amp;src=MmpzMzhoMWFiamNnYTQxYjlvOG1zb2czaHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%238A2D38&amp;showTz=0&amp;showCalendars=0&amp;showTabs=1&amp;showNav=1&amp;showDate=1&amp;mode=WEEK" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23EF6C00&amp;ctz=Europe%2FParis&amp;src=b3QzdHZtMmZqMmZrNGYzNDZ1anEzN2E1bmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%239F3501&amp;showTz=0&amp;showCalendars=0&amp;mode=WEEK" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </div>
 
     <div class="agenda"> <!-- Agenda des événements organisés par l'ALE -->
-
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23EF6C00&amp;ctz=Europe%2FParis&amp;showTz=0&amp;showCalendars=0&amp;mode=WEEK&amp;src=aW9lZ284czlmczdqY2c1ZTg1YnZ1bWI2M2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23DD5511" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </div>
 
     <?php include "Design/Footer.view.php"; ?>
