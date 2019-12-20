793003a5fe0841a045a132224cfc56d3ba0ac6a3
xiensy <richomme.lucas@gmail.com>
2019-12-20 14:02:26 +0100

    Laisse52 5m5poi

--- WEB/mvc/BD/create.sql
+++ WEB/mvc/BD/create.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS Contact(
-  ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  Nom TEXT,
-  Prenom TEXT,
-  Adresse TEXT,
-  NumTel TEXT,
-  Mail TEXT
+  ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Nom TEXT(100),
+  Prenom TEXT(100),
+  Adresse TEXT(100),
+  NumTel TEXT(100),
+  Mail TEXT(100)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS Utilisateur(
@@ -15,12 +15,12 @@ CREATE TABLE IF NOT EXISTS Utilisateur(
   Prenom varchar(100),
   Genre ENUM('H','F'),
   DateNaissance DATE,
-  Adresse TEXT,
-  NumTel TEXT,
-  NumFix TEXT,
-  Mail TEXT,
+  Adresse TEXT(100),
+  NumTel TEXT(100)(100),
+  NumFix TEXT(100),
+  Mail TEXT(100),
   Role ENUM ('Bureau', 'Entraineur', 'Administrateur', 'Adherent', 'Mineur', 'Benevole'),
-  CodeUtilisateur TEXT,
+  CodeUtilisateur TEXT(100),
   Passeport ENUM('Blanc', 'Jaune', 'Orange', 'Vert', 'Bleu', 'RougePerf', 'RougeExt','Noir'),
   Contact INT,
   CONSTRAINT `fk_contact_contactid`
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS Utilisateur(
 CREATE TABLE IF NOT EXISTS CompteRendu(
   Titre varchar(50),
   DatePub DATE,
-  Contenu TEXT,
+  Contenu TEXT(100),
   NumAuteur INT,
   FOREIGN KEY (NumAuteur) REFERENCES Utilisateur(ID),
   PRIMARY KEY (Titre, DatePub)
@@ -42,27 +42,27 @@ CREATE TABLE IF NOT EXISTS Message(
   NumExp INT,
   NumDest INT,
   DateEnv TIMESTAMP,
-  Contenu TEXT,
+  Contenu TEXT(100),
   PRIMARY KEY (NumExp, NumDest, DateEnv),
   FOREIGN KEY (NumExp) REFERENCES Utilisateur(ID),
   FOREIGN KEY (NumDest) REFERENCES Utilisateur(ID)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS Lieu(
-  Nom TEXT PRIMARY KEY,
-  Adresse TEXT,
+  Nom TEXT(100) PRIMARY KEY,
+  Adresse TEXT(100),
   Categorie ENUM ('Interieur', 'Exterieur')
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS Event(
   ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  Nom TEXT,
+  Nom TEXT(100),
   DateDebut TIMESTAMP,
   DateFin TIMESTAMP CHECK (DateFin > DateDebut),
-  Description TEXT,
+  Description TEXT(100),
   Officiel BOOLEAN,
   NumCrea INT,
-  NomLieu TEXT,
+  NomLieu TEXT(100),
   FOREIGN KEY (NumCrea) REFERENCES Utilisateur(ID),
   FOREIGN KEY (NomLieu) REFERENCES Lieu(Nom)
 ) ENGINE = InnoDB;
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS Sujet(
   ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   Titre VARCHAR(50),
   DatePub DATE,
-  Contenu TEXT,
+  Contenu TEXT(100),
   IDAuteur INT,
   IDEvent INT default null,
   FOREIGN KEY (IDAuteur) REFERENCES Utilisateur(ID),
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS Certificat(
   ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   NumLic INT,
   Type ENUM ('L','C','PSS','NP'),
-  NomMedecin Text,
+  NomMedecin TEXT(100),
   DateSaisie Date,
   Alpi BOOLEAN,
   FOREIGN KEY (NumLic) REFERENCES Utilisateur(ID)
@@ -103,12 +103,12 @@ CREATE TABLE IF NOT EXISTS Pratique (
 
 CREATE TABLE IF NOT EXISTS Cours(
   ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  Nom TEXT,
+  Nom TEXT(100),
   HeureDebut TIME,
   HeureFin TIME CHECK (HeureFin > HeureDebut),
   Jour ENUM('L','M','Me','J','V','S','D'),
   NbPlace INT,
-  NomLieu TEXT,
+  NomLieu TEXT(100),
   NumEntraineur INT,
   FOREIGN KEY (NomLieu) REFERENCES Lieu(Nom),
   FOREIGN KEY (NumEntraineur) REFERENCES Utilisateur(ID)
@@ -118,7 +118,7 @@ CREATE TABLE IF NOT EXISTS Commentaire(
   NumAuteur INT,
   IDSujet INT,
   Date DATE,
-  Contenu TEXT,
+  Contenu TEXT(100),
   PRIMARY KEY (NumAuteur, IDSujet, Date),
   FOREIGN KEY (NumAuteur) REFERENCES Utilisateur(ID),
   FOREIGN KEY (IDSujet) REFERENCES Sujet(ID)
