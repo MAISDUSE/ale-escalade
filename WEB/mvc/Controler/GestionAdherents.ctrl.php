d2abb26e23e067273edebd75d9f0bac6bd8c85ff
xiensy <richomme.lucas@gmail.com>
2020-01-10 14:28:21 +0100

    Gestion des adhérents

--- WEB/mvc/Controler/GestionAdherents.ctrl.php
+++ WEB/mvc/Controler/GestionAdherents.ctrl.php
@@ -5,5 +5,18 @@ require_once("../Framework/View.class.php");
 
 if(isset($_POST['viewUser'])) {
   $view = new View("Adherent");
-  $view->adherent;
-} ?>
+  $db = new DAO;
+  $view->adherent = $db->getUserByCode($_POST['idUser']);
+  $view->afficher();
+} else if (isset($_POST['modifUser'])){
+  $view = new View("ModificationAdherent");
+  $db = new DAO;
+  $view->adherent = $db->getUserByCode($_POST['idUser']);
+  $view->afficher();
+} else if (isset($_POST['supprUser'])){
+  $db = new DAO;
+  $db->deleteUserById($_POST['idUser']);
+  $view = new View("GestionAdherents");
+  $view->adherents = $db->getAllUsers();
+  $view->afficher();
+}?>
