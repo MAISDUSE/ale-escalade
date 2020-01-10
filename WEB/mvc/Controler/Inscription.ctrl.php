f58827560563642d2f60fbe0f52482048e27b0db
HugoDeku <hucureuil@gmail.com>
2020-01-10 16:57:30 +0100

    Wesh

--- WEB/mvc/Controler/Inscription.ctrl.php
+++ WEB/mvc/Controler/Inscription.ctrl.php
@@ -7,10 +7,10 @@ require_once("../Framework/View.class.php");
 
 if(isset($_POST['age'])){
   var_dump($_POST);
-  $date = date("Y-m-d", strtotime($_POST['age']);
+  //$date = date("Y-m-d", strtotime($_POST['age']));
   $db = new DAO;
-  $db->addInscription($_POST['nom'], $_POST['prenom'], $_POST['sexe'], $_POST['assurance']
-                      ,$date, $_POST['adresse'],$_POST['passeport'] $_POST['codepostal'], $_POST['mail']
+  $db->addInscription($_POST['nom'], $_POST['prenom'], $_POST['sexe'], $_POST['base']
+                      ,$_POST['age'], $_POST['adresse'],$_POST['passeport'], $_POST['codepostal'], $_POST['mail']
                       ,$_POST['tel'], $_POST['nomContact'], $_POST['prenomContact']
                       ,$_POST['telContact'], $_POST['adresseContact'], $_POST['mailContact']);
 
