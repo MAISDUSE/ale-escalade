1df5a1ca0cbe7ad4b96dfbff9c2f8ba3ac545b35
BuildTools <unconfigured@null.spigotmc.org>
2020-01-12 13:14:29 +0100

    Indentation

--- WEB/mvc/Controler/GestionActualites.ctrl.php
+++ WEB/mvc/Controler/GestionActualites.ctrl.php
@@ -4,43 +4,43 @@ require_once("../Model/DAO.class.php");
 require_once("../Framework/View.class.php");
 
 if(isset($_SESSION['user']) && $_SESSION['user']->isAdmin() == "TRUE"){
-if(isset($_POST['viewAuteur'])){
-  //Affichage du profil de l'auteur
-  $db = new DAO;
+  if(isset($_POST['viewAuteur'])){
+    //Affichage du profil de l'auteur
+    $db = new DAO;
 
-  $view = new View("Adherent");
-  $view->adherent = $db->getAdherentByCode($_POST['idAuteur']);
-  $view->afficher();
-} else if (isset($_POST['viewActualite'])){
-  //Affichage de l'actualité
-  $db = new DAO;
+    $view = new View("Adherent");
+    $view->adherent = $db->getAdherentByCode($_POST['idAuteur']);
+    $view->afficher();
+  } else if (isset($_POST['viewActualite'])){
+    //Affichage de l'actualité
+    $db = new DAO;
 
-  $view = new View("Actualite");
-  $view->actualite = $db->getActualiteById($_POST['idActualite']);
-  $view->afficher();
-} else if (isset($_POST['modifActualite'])){
-  //Affichage de la page de modification de l'actualité
-  $db = new DAO;
+    $view = new View("Actualite");
+    $view->actualite = $db->getActualiteById($_POST['idActualite']);
+    $view->afficher();
+  } else if (isset($_POST['modifActualite'])){
+    //Affichage de la page de modification de l'actualité
+    $db = new DAO;
 
-  $view = new View("ModificationActualite");
-  $view->actualite = $db->getActualiteById($_POST['idActualite']);
-  $view->afficher();
-} else if (isset($_POST['supprActualite'])){
-  //Suppression de l'actualité
-  $db = new DAO;
-  $db->deleteActualiteById($_POST['idActualite']);
+    $view = new View("ModificationActualite");
+    $view->actualite = $db->getActualiteById($_POST['idActualite']);
+    $view->afficher();
+  } else if (isset($_POST['supprActualite'])){
+    //Suppression de l'actualité
+    $db = new DAO;
+    $db->deleteActualiteById($_POST['idActualite']);
 
-  $view = new View("GestionActualites");
-  $view->actualites = $db->getAllActualite();
-  $view->afficher();
-} else {
-  //Affichage normal de la page
-  $db = new DAO;
+    $view = new View("GestionActualites");
+    $view->actualites = $db->getAllActualite();
+    $view->afficher();
+  } else {
+    //Affichage normal de la page
+    $db = new DAO;
 
-  $view = new View("GestionActualites");
-  $view->actualites = $db->getAllActualite();
-  $view->afficher();
-}
+    $view = new View("GestionActualites");
+    $view->actualites = $db->getAllActualite();
+    $view->afficher();
+  }
 } else {
   $_SESSION['erreur'] = new Retour(NULL, TRUE, "Il faut être connecté et être administrateur pour accéder à cette page");
   $view = new View("../Controler/Accueil.ctrl.php");
--- WEB/mvc/Controler/GestionAdherents.ctrl.php
+++ WEB/mvc/Controler/GestionAdherents.ctrl.php
@@ -4,36 +4,36 @@ require_once("../Model/DAO.class.php");
 require_once("../Framework/View.class.php");
 
 if(isset($_SESSION['user']) && $_SESSION['user']->isAdmin() == "TRUE"){
-if(isset($_POST['viewUser'])) {
-  //Vue du profil de l'adhérent
-  $db = new DAO;
+  if(isset($_POST['viewUser'])) {
+    //Vue du profil de l'adhérent
+    $db = new DAO;
 
-  $view = new View("Adherent");
-  $view->adherent = $db->getAdherentByCode($_POST['idUser']);
-  $view->afficher();
-} else if (isset($_POST['modifUser'])){
-  //Vue de modification de l'adhérent
-  $db = new DAO;
+    $view = new View("Adherent");
+    $view->adherent = $db->getAdherentByCode($_POST['idUser']);
+    $view->afficher();
+  } else if (isset($_POST['modifUser'])){
+    //Vue de modification de l'adhérent
+    $db = new DAO;
 
-  $view = new View("ModificationAdherent");
-  $view->adherent = $db->getAdherentByCode($_POST['idUser']);
-  $view->afficher();
-} else if (isset($_POST['supprUser'])){
-  //Suppression de l'adhérent et refresh de la page
-  $db = new DAO;
-  $db->deleteUserById($_POST['idUser']);
+    $view = new View("ModificationAdherent");
+    $view->adherent = $db->getAdherentByCode($_POST['idUser']);
+    $view->afficher();
+  } else if (isset($_POST['supprUser'])){
+    //Suppression de l'adhérent et refresh de la page
+    $db = new DAO;
+    $db->deleteUserById($_POST['idUser']);
 
-  $view = new View("GestionAdherents");
-  $view->adherents = $db->getAllAdherent();
-  $view->afficher();
-} else {
-  //Affichage normal de la page
-  $db = new DAO;
+    $view = new View("GestionAdherents");
+    $view->adherents = $db->getAllAdherent();
+    $view->afficher();
+  } else {
+    //Affichage normal de la page
+    $db = new DAO;
 
-  $view = new View("GestionAdherents");
-  $view->adherents = $db->getAllAdherent();
-  $view->afficher();
-}
+    $view = new View("GestionAdherents");
+    $view->adherents = $db->getAllAdherent();
+    $view->afficher();
+  }
 } else {
   $_SESSION['erreur'] = new Retour(NULL, TRUE, "Il faut être connecté et être administrateur pour accéder à cette page");
   $view = new View("../Controler/Accueil.ctrl.php");
