b181c1a8f52f13b9553b97f8529488190302e2c7
gherrazs <salah.gherraz@iut2.univ-grenoble-alpes.fr>
2020-01-12 16:48:16 +0100

    Reglage pb

--- WEB/mvc/Controler/GestionInscriptions.ctrl.php
+++ WEB/mvc/Controler/GestionInscriptions.ctrl.php
@@ -1,8 +1,11 @@
 <?php
 require_once("../Model/Utilisateur.class.php");
-//require_once("../Model/Inscription.class.php"); A décommenter une fois créée
 require_once("../Model/DAO.class.php");
 require_once("../Framework/View.class.php");
+session_start();
+
+//require_once("../Model/Inscription.class.php"); A décommenter une fois créée
+
 if(isset($_SESSION['user']) && $_SESSION['user']->isAdmin() == "TRUE"){
   if(isset($_POST['accepter'])){
     //Acceptation de l'inscription, on ajoute l'adhérent et suppression de l'inscription
