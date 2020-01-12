5f1ef00816b866fec459414cb4025b16609f4e5e
BuildTools <unconfigured@null.spigotmc.org>
2020-01-12 14:31:43 +0100

    Correction DAO et Ajout vue profil adherent

--- WEB/mvc/Model/DAO.class.php
+++ WEB/mvc/Model/DAO.class.php
@@ -321,15 +321,10 @@ function getEventOfficial(){
   $lancement = $requete->fetchAll(PDO::FETCH_CLASS, 'Evenement');
   return array($lancement);
  }
-<<<<<<< HEAD
-  function addEvenement( $nom, $img, $dateCreation,
-                       $dateDebut, $dateFin, $description,$officiel,
-                       $numCrea, $nomLieu){
-=======
+
 function addEvenement( string $nom, string $img, string $dateCreation,
                          string $dateDebut, string $dateFin, string $description,
                          int $numCrea,string $nomLieu, bool $officiel){
->>>>>>> a7efe4881f766733cc778c116a22282b266e3675
 
     $req ="INSERT INTO Event(Nom,Image,DatePub,DateDebut,DateFin,Description,Officiel,NumCrea,NomLieu)
     VALUES(:nom,:image,:datePub,:dateDeb,:dateFin,:description,:officiel,:numCrea,:lieu)";
@@ -345,7 +340,7 @@ function addEvenement( string $nom, string $img, string $dateCreation,
                         ':numCrea' => $numCrea,
                         ':lieu'=> $nomLieu));
 
-    }
+}
 
 
 function addInscription($nom, $prenom, $sexe, $assurance, $datedenaissance, $adresse, $codepostal, $adressemail, $passeport, $numtel
